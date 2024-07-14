5baba554162359e3d04e1b876eab76c8209fd40e
Behruz <madaminovbehruz746@gmail.com>
2024-07-14 17:26:00 +0500

    added: files

---
+++ src/components/card/GifCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function GifCard() {
+  return <div>GifCard</div>;
+}
+
+export default GifCard;
---
+++ src/layout/MainRoot.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MainRoot() {
+  return (
+    <div>MainRoot</div>
+  )
+}
+
+export default MainRoot
